d4f10696a22425c91abf14d7b170b081786c079a
Luis Love <luisl.love25@gmail.com>
2020-06-30 02:41:43 -0500

    Added support for logging throughout the project.

---
+++ lib/logger.sh
@@ -0,0 +1,148 @@
+#
+# dotfiles/lib/logger.sh
+#
+# Contains various logging functions.
+#
+# Author: Luis Love
+#
+
+# Directory containing all logs from this project
+declare -r LOG_DIR="$HOME/.dotfiles/log"
+
+# Log file names
+declare -r LOG_APT="apt.log"
+declare -r LOG_BREW="brew.log"
+declare -r LOG_DOTFILES="dotfiles.log"
+declare -r LOG_INSTALL="install.log"
+
+# Regex of a valid name for a directory containing the above log files
+declare -r VALID_LOG_ID_REGEX='^log_[0-9]{2}-[0-9]{2}-[0-9]{4}_[0-9]{2}-[0-9]{2}-[0-9]{2}$'
+
+
+createLogID() {
+    local logID="log_$(date +"%m-%d-%Y_%H-%M-%S" 2> /dev/null)"
+
+    [[ "$logID" == "log_" ]] && return 1
+    echo "$logID"
+}
+
+
+validLogID() {
+    [[ "$1" =~ $VALID_LOG_ID_REGEX ]]
+}
+
+
+_logExists() {
+    local logFile="$LOG_DIR/$1/$2"
+
+    [[ -f "$logFile" && -r "$logFile" && -w "$logFile" ]]
+}
+
+
+logAptExists() {
+    _logExists "$1" "$LOG_APT"
+}
+
+
+logBrewExists() {
+    _logExists "$1" "$LOG_BREW"
+}
+
+
+logDotfilesExists() {
+    _logExists "$1" "$LOG_DOTFILES"
+}
+
+
+logInstallExists() {
+    _logExists "$1" "$LOG_INSTALL"
+}
+
+
+_createLog() {
+    local fileDir="$LOG_DIR/$1"
+    local file="$fileDir/$2"
+
+    if _logExists "$1" "$2"; then
+        return 0
+    fi
+
+    # Creates logging directory, if it doesn't exist
+    if [[ ! -d "$fileDir" ]]; then
+        if ! mkdir -p "$fileDir"; then
+            return 1
+        fi
+    fi
+    
+    # Creates log file, if it doesn't exist
+    if [[ ! -f "$file" ]]; then
+        rm -rf "$file"
+        if ! (touch "$file" && chmod u+rw "$file"); then
+            return 1
+        fi
+    fi
+
+    return 0
+}
+
+
+createLogApt() {
+    _createLog "$1" "$LOG_APT"
+}
+
+
+createLogBrew() {
+    _createLog "$1" "$LOG_BREW"
+}
+
+
+createLogDotfiles() {
+    _createLog "$1" "$LOG_DOTFILES"
+}
+
+
+createLogInstall() {
+    _createLog "$1" "$LOG_INSTALL"
+}
+
+
+_logMessage() {
+    local fileDir="$LOG_DIR/$1"
+    local file="$fileDir/$2"
+    local message="$3"
+    local printMessage="$4"
+
+    if ! _createLog "$1" "$2"; then
+        return 1
+    fi
+
+    if "$printMessage"; then
+        printf "$message"
+    fi
+
+    local line=""
+    while IFS= read -r line; do
+        [[ ! "$line" == *\n ]] && line="$line\n"
+        printf "$(date +"%m-%d-%Y (%H:%M:%S)"): $line" >> "$file"	
+    done <<< "$message"
+}
+
+
+logApt() {
+    _logMessage "$1" "$LOG_APT" "$2" "$3"
+}
+
+
+logBrew() {
+    _logMessage "$1" "$LOG_BREW" "$2" "$3"
+}
+
+
+logDotfiles() {
+    _logMessage "$1" "$LOG_DOTFILES" "$2" "$3"
+}
+
+
+logInstall() {
+    _logMessage "$1" "$LOG_INSTALL" "$2" "$3"
+}

bfc291364b3ecf40d54931361a8bc3a7325f9bdd
Luis Love <luisl.love25@gmail.com>
2020-07-05 15:56:13 -0500

    Added installation script for packages installed using the APT package manager. Under construction.

---
+++ lib/apt.sh
@@ -0,0 +1,9 @@
+#
+# dotfiles/lib/apt.sh
+#
+# Contains various functions for aiding in the installation and processing
+# of programs from linux distros using the APT package manager.
+#
+# Author: Luis Love
+#
+

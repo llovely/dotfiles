1a633f41bbec11599cab123d25a35a608c0ef3bf
Luis Love <luisl.love25@gmail.com>
2022-06-17 02:21:52 -0500

    Created utility functions to display text to stdout/stderr.

---
+++ src/utils/message.sh
@@ -0,0 +1,43 @@
+#
+# Functions for managing output message(s) to stdout and stderr.
+#
+
+# Globals (these variable(s) can be set by the user of these utility functions)
+DISPLAY_OUTPUT_MSG='true'
+DISPLAY_ERROR_MSG='true'
+
+
+################################################################################
+# Writes message(s) to stderr.
+# Globals:
+#   None
+# Arguments:
+#   Message to display (string)
+# Outputs:
+#   Writes message(s) to stderr.
+# Returns:
+#   0 if message(s) display successfully; otherwise, non-zero on error.
+################################################################################
+function err_msg() {
+  local messages="$*"
+
+  [[ "${DISPLAY_ERROR_MSG}" == 'true' ]] && printf "${messages}\n" >&2
+}
+
+
+################################################################################
+# Writes message(s) to stdout.
+# Globals:
+#   None
+# Arguments:
+#   Message to display (string)
+# Outputs:
+#   Writes message(s) to stdout.
+# Returns:
+#   0 if message(s) display successfully; otherwise, non-zero on error.
+################################################################################
+function out_msg() {
+  local messages="$*"
+  
+  [[ "${DISPLAY_OUTPUT_MSG}" == 'true' ]] && printf "${messages}\n"
+}

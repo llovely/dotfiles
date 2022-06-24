08a1fc01479373aa6991503a8b7773edbd74409c
Luis Love <luisl.love25@gmail.com>
2022-06-24 10:55:23 -0500

    Created utility functions to obtain user confirmation.

---
+++ src/utils/confirmation.sh
@@ -0,0 +1,47 @@
+#
+# Functions for obtaining user confirmation.
+#
+
+# Globals (these variable(s) SHOULD NOT be modified outside of this utility)
+CONFIRMATION_RESPONSE=''
+
+
+################################################################################
+# Obtains user confirmation.
+# Globals:
+#   FUNCNAME
+#   CONFIRMATION_RESPONSE
+# Arguments:
+#   Message(s) to display (string)
+# Outputs:
+#   Writes message(s) to stdout; writes error message(s) to stderr.
+# Returns:
+#   0 if confirmation obtained; otherwise, non-zero on error.
+################################################################################
+function seek_confirmation() {
+  local messages="$*"
+  local prompt="Are you sure you want to continue? [Y/n]: "
+
+  printf "${messages}\n"
+  if ! read -r -p "${prompt}" CONFIRMATION_RESPONSE; then
+    echo "ERROR: ${FUNCNAME[0]}() failed. Unable to obtain" \
+         "confirmation." >&2
+    return 1
+  fi
+}
+
+
+################################################################################
+# Indicates if confirmation is confirmed.
+# Globals:
+#   CONFIRMATION_RESPONSE
+# Arguments:
+#   None
+# Outputs:
+#   None
+# Returns:
+#   0 if confirmation confirmed; otherwise, non-zero.
+################################################################################
+function is_confirmed() {
+  [[ "${CONFIRMATION_RESPONSE}" =~ ^[Yy].* ]] &> /dev/null
+}
